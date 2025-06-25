fcb25d8d9399c75fb4c1897e03e039cc6371ca66
Mir Yeasir Abrar <182193318+MirYeasirAbrar@users.noreply.github.com>
2025-06-25 21:32:55 +0600

    Sheet 01 - A_Say_Hello_With_C

---
+++ Assuit ICPC/Sheet 01/A_Say_Hello_With_C.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    char a[10000]; cin >> a;
+    cout << "Hello, " << a << endl;
+    return 0;
+}
