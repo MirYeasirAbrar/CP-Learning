da8182427eb1a303e3544f45df10db82448a56b8
Mir Yeasir Abrar <182193318+MirYeasirAbrar@users.noreply.github.com>
2025-06-26 20:24:01 +0600

    381A - Sereja and Dima

---
+++ Assuit ICPC/Sheet 01/test.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+int arrayReturn(){
+
+    //int a[] = {1,2,3,4,5};
+
+    //cout << "hello" << endl;
+    int x = 5;
+    return x;
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    cout << arrayReturn() << endl;
+    
+    return 0;
+}
